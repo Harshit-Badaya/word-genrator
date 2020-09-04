d462a3696310ac1deb5105cee08c101aaf08d535
Harshit <harshitbadaya95@gmail.com>
2020-09-04 16:10:08 +0530

    Word genrating worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import arrayWords from "../utlis/words"
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,7 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
-
-
+  
   words = '';
+  limit = 10;
+
+
+handleSlideChange(newLimit : number){
+  this.limit = newLimit;
+}
+
+
+genrate(){
+  this.words = arrayWords.slice(0,this.limit).join(" ");
+  console.log(this.words);
+} 
+
 }
